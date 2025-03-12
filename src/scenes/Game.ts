1b8df0ae5679f59d9c94ea1cf891ee5032066467
debora <debora.delvecchio@doctolib.com>
2025-03-12 12:15:19 +0100

    finish game

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,146 +1,198 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
 export class Game extends Scene {
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    backgrounds: Phaser.GameObjects.Image[];
-    msg_text: Phaser.GameObjects.Text;
-    ground: Phaser.GameObjects.Image;
-    dino!: Phaser.GameObjects.Sprite;
-    cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
-    asteroids!: Phaser.Physics.Arcade.Group;
-    asteroidKeys: string[];    
+  camera: Phaser.Cameras.Scene2D.Camera;
+  background: Phaser.GameObjects.Image;
+  backgrounds: Phaser.GameObjects.Image[];
+  msg_text: Phaser.GameObjects.Text;
+  ground: Phaser.GameObjects.Image;
+  dino!: Phaser.GameObjects.Sprite;
+  cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  asteroids!: Phaser.Physics.Arcade.Group;
+  asteroidKeys: string[];
+  food!: Phaser.Physics.Arcade.Group;
+  foodKeys: string[];
+  score: number = 0;
 
-    constructor() {
-        super('Game');
-        this.asteroidKeys = ["small_asteroid", "large_asteroid", 'asteroid']; // Define asteroid keys
+  constructor() {
+    super("Game");
+    this.asteroidKeys = ["small_asteroid", "large_asteroid", "asteroid"]; // Define asteroid keys
+    this.foodKeys = [
+      "fish",
+      "steak",
+      "jerky",
+      "star",
+      "bacon",
+      "star",
+      "star",
+      "star",
+    ]; // Define food keys
+  }
+
+  create() {
+    this.camera = this.cameras.main;
+    this.backgrounds = [];
+    // this.camera.setBackgroundColor(0x00ff00);
+
+    // Add multiple background layers
+    const backgroundLayers = ["background_0", "background_1", "background_2"];
+
+    backgroundLayers.forEach((key, index) => {
+      const bg = this.add
+        .image(0, 0, key)
+        .setOrigin(0, 0)
+        .setDisplaySize(this.cameras.main.width, this.cameras.main.height)
+        .setDepth(index); // Set layer order
+
+      this.backgrounds.push(bg);
+    });
+
+    // Optional: Adjust alpha for each layer
+    this.backgrounds[0].setAlpha(1); // Back layer
+    this.backgrounds[1].setAlpha(0.8); // Middle layer
+    this.backgrounds[2].setAlpha(0.6);
+
+    this.ground = this.add
+      .image(0, this.cameras.main.height, "tileset")
+      .setOrigin(0, 1) // Anchor to bottom-left
+      .setDisplaySize(this.cameras.main.width, 100);
+
+    this.ground.setDepth(3);
+
+    this.anims.create({
+      key: "run",
+      frames: this.anims.generateFrameNumbers("dino", {
+        start: 0,
+        end: 3,
+      }),
+      frameRate: 10,
+      repeat: -1, // -1 means loop forever
+    });
+
+    const groundTopCenterPosition = this.ground.getTopCenter();
+
+    // Create sprite and play animation
+    this.dino = this.physics.add
+      .sprite(groundTopCenterPosition.x, groundTopCenterPosition.y, "dino")
+      .setScale(3)
+      .setDepth(4);
+
+    this.dino.play("run");
+    this.cursors = this.input.keyboard?.createCursorKeys();
+
+    // Create a group for asteroids
+    this.asteroids = this.physics.add.group({
+      defaultKey: "large_asteroid",
+    });
+
+    this.food = this.physics.add.group({
+      defaultKey: "star",
+    });
+
+    // Spawn asteroids at random intervals
+    this.time.addEvent({
+      delay: 1000,
+      callback: this.spawnAsteroid,
+      callbackScope: this,
+      loop: true,
+    });
+
+    // Spawn food at random intervals
+    this.time.addEvent({
+      delay: 2000,
+      callback: this.spawnFood,
+      callbackScope: this,
+      loop: true,
+    });
+
+    this.input.once("pointerdown", () => {
+      this.scene.start("GameOver");
+    });
+
+    this.physics.add.overlap(
+      this.dino,
+      this.asteroids,
+      this.handleCollision,
+      null,
+      this,
+    );
+
+    this.physics.add.overlap(
+      this.dino,
+      this.food,
+      this.handleFoodCollision,
+      null,
+      this,
+    );
+  }
+
+  spawnAsteroid() {
+    const x = Phaser.Math.Between(0, this.cameras.main.width);
+    const key = Phaser.Math.RND.pick(this.asteroidKeys);
+    const asteroid = this.asteroids.get(x, 0, key);
+
+    if (asteroid) {
+      asteroid.setActive(true);
+      asteroid.setVisible(true);
+      asteroid.setVelocityY(200);
+      asteroid.setDepth(4);
+      asteroid.setVelocity(
+        Phaser.Math.Between(-100, 100), // x velocity for some variety
+        Phaser.Math.Between(150, 300), // y velocity (falling speed)
+      );
+      // Add rotation
+      asteroid.setAngularVelocity(Phaser.Math.Between(-100, 100));
+      asteroid.setScale(Phaser.Math.RND.pick([2, 3]));
+    }
+  }
+
+  spawnFood() {
+    const x = Phaser.Math.Between(0, this.cameras.main.width);
+    const key = Phaser.Math.RND.pick(this.foodKeys);
+    const food = this.food.get(x, 0, key);
+
+    if (food) {
+      food.setActive(true);
+      food.setVisible(true);
+      food.setVelocityY(200);
+      food.setDepth(4);
+      food.setScale(Phaser.Math.RND.pick([2, 3]));
+    }
+  }
+
+  update() {
+    const speed = 4;
+    // Calculate boundaries based on sprite width and screen width
+    const leftBound = this.dino.displayWidth / 2;
+    const rightBound = this.cameras.main.width - this.dino.displayWidth / 2;
+
+    if (this.cursors.left.isDown && this.dino.x > leftBound) {
+      this.dino.x -= speed;
+      this.dino.setFlipX(true);
+    } else if (this.cursors.right.isDown && this.dino.x < rightBound) {
+      this.dino.x += speed;
+      this.dino.setFlipX(false);
     }
 
-    create() {
-        this.camera = this.cameras.main;
-        this.backgrounds = [];
-        // this.camera.setBackgroundColor(0x00ff00);
+    // this.physics.world.collide(this.dino, this.asteroids, this.handleCollision, undefined, this);
+  }
 
-        // Add multiple background layers
-        const backgroundLayers = ['background_0', 'background_1', 'background_2'];
+  handleFoodCollision(
+    dino: Phaser.GameObjects.Sprite,
+    food: Phaser.GameObjects.Image,
+  ) {
+    this.score += 1;
+    // make food disappear
 
-        backgroundLayers.forEach((key, index) => {
-            const bg = this.add.image(0, 0, key)
-                .setOrigin(0, 0)
-                .setDisplaySize(
-                    this.cameras.main.width,
-                    this.cameras.main.height
-                )
-                .setDepth(index); // Set layer order
+    food.destroy();
+  }
 
-            this.backgrounds.push(bg);
-        });
-
-        // Optional: Adjust alpha for each layer
-        this.backgrounds[0].setAlpha(1);    // Back layer
-        this.backgrounds[1].setAlpha(0.8);  // Middle layer
-        this.backgrounds[2].setAlpha(0.6);
-
-
-        this.ground = this.add.image(0, this.cameras.main.height, 'tileset')
-            .setOrigin(0, 1)  // Anchor to bottom-left
-            .setDisplaySize(this.cameras.main.width, 100);
-
-        this.ground.setDepth(3);
-
-        this.anims.create({
-            key: 'run',
-            frames: this.anims.generateFrameNumbers('dino', {
-                start: 0,
-                end: 3
-            }),
-            frameRate: 10,
-            repeat: -1  // -1 means loop forever
-        });
-
-        const groundTopCenterPosition = this.ground.getTopCenter();
-
-        // Create sprite and play animation
-        this.dino = this.physics.add.sprite(groundTopCenterPosition.x, groundTopCenterPosition.y, 'dino').setScale(3).setDepth(4);
-
-        this.dino.play('run');
-        this.cursors = this.input.keyboard?.createCursorKeys();
-
-        // Create a group for asteroids
-        this.asteroids = this.physics.add.group({
-            defaultKey: 'large_asteroid',
-            maxSize: 10
-        });
-
-
-
-        // Spawn asteroids at random intervals
-        this.time.addEvent({
-            delay: 1000,
-            callback: this.spawnAsteroid,
-            callbackScope: this,
-            loop: true
-        });
-
-
-
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('GameOver');
-
-        });
-
-        this.physics.add.overlap(
-            this.dino,
-            this.asteroids,
-            this.handleCollision,
-            null,
-            this
-          );
-    }
-
-
-    spawnAsteroid() {
-        const x = Phaser.Math.Between(0, this.cameras.main.width);
-        const key = Phaser.Math.RND.pick(this.asteroidKeys);
-        const asteroid = this.asteroids.get(x, 0, key);
-
-        if (asteroid) {
-            asteroid.setActive(true);
-            asteroid.setVisible(true);
-            asteroid.setVelocityY(200);
-            asteroid.setDepth(4);
-            asteroid.setVelocity(
-                Phaser.Math.Between(-100, 100), // x velocity for some variety
-                Phaser.Math.Between(150, 300) // y velocity (falling speed)
-              );
-              // Add rotation
-    asteroid.setAngularVelocity(Phaser.Math.Between(-100, 100));
-
-        }
-    }
-
-    update() {
-        const speed = 4;
-        // Calculate boundaries based on sprite width and screen width
-        const leftBound = this.dino.displayWidth / 2;
-        const rightBound = this.cameras.main.width - (this.dino.displayWidth / 2);
-
-        if (this.cursors.left.isDown && this.dino.x > leftBound) {
-            this.dino.x -= speed;
-            this.dino.setFlipX(true);
-        }
-        else if (this.cursors.right.isDown && this.dino.x < rightBound) {
-            this.dino.x += speed;
-            this.dino.setFlipX(false);
-        }
-
-        // this.physics.world.collide(this.dino, this.asteroids, this.handleCollision, undefined, this);
-    }
-
-    handleCollision(dino: Phaser.GameObjects.Sprite, asteroid: Phaser.GameObjects.Image) {
-        // Handle collision (e.g., end game, reduce health, etc.)
-        // Stop game
+  handleCollision(
+    dino: Phaser.GameObjects.Sprite,
+    asteroid: Phaser.GameObjects.Image,
+  ) {
+    // Handle collision (e.g., end game, reduce health, etc.)
+    // Stop game
     this.gameOver = true;
 
     // Stop player animation and movement
@@ -163,12 +215,12 @@ export class Game extends Scene {
           stroke: "#000000",
           strokeThickness: 6,
           align: "center",
-        }
+        },
       )
       .setOrigin(0.5)
       .setDepth(10);
 
     // Shake camera for impact effect
     this.cameras.main.shake(500, 0.05);
-    }
+  }
 }
--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -1,55 +1,56 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class Preloader extends Scene
-{
-    constructor ()
-    {
-        super('Preloader');
-    }
+export class Preloader extends Scene {
+  constructor() {
+    super("Preloader");
+  }
 
-    init ()
-    {
-        //  We loaded this image in our Boot Scene, so we can display it here
-        this.add.image(512, 384, 'background');
+  init() {
+    //  We loaded this image in our Boot Scene, so we can display it here
+    this.add.image(512, 384, "background");
 
-        //  A simple progress bar. This is the outline of the bar.
-        this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+    //  A simple progress bar. This is the outline of the bar.
+    this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
 
-        //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
+    //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
+    const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
 
-        //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
-        this.load.on('progress', (progress: number) => {
+    //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
+    this.load.on("progress", (progress: number) => {
+      //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
+      bar.width = 4 + 460 * progress;
+    });
+  }
 
-            //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-            bar.width = 4 + (460 * progress);
+  preload() {
+    //  Load the assets for the game - Replace with your own assets
+    this.load.setPath("assets");
 
-        });
-    }
+    this.load.image("logo", "logo.png");
 
-    preload ()
-    {
-        //  Load the assets for the game - Replace with your own assets
-        this.load.setPath('assets');
+    this.load.image("background_0", "stringstar-fields/background_0.png");
+    this.load.image("background_1", "stringstar-fields/background_1.png");
+    this.load.image("background_2", "stringstar-fields/background_2.png");
+    this.load.image("tileset", "stringstar-fields/tile.png");
+    this.load.image("asteroid", "asteroids/asteroid.png");
+    this.load.image("small_asteroid", "asteroids/small_asteroid.png");
+    this.load.image("large_asteroid", "asteroids/large_asteroid.png");
+    this.load.spritesheet("dino", "dino-characters/sheets/dino.png", {
+      frameWidth: 24,
+      frameHeight: 24,
+    });
+    this.load.image("fish", "Fish.png");
+    this.load.image("steak", "Steak.png");
+    this.load.image("bacon", "Bacon.png");
+    this.load.image("jerky", "Jerky.png");
+    this.load.image("star", "star.png");
+  }
 
-        this.load.image('logo', 'logo.png');
+  create() {
+    //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
+    //  For example, you can define global animations here, so we can use them in other scenes.
 
-        this.load.image("background_0", 'stringstar-fields/background_0.png');
-        this.load.image("background_1", 'stringstar-fields/background_1.png');
-        this.load.image("background_2", 'stringstar-fields/background_2.png');
-        this.load.image("tileset", 'stringstar-fields/tile.png');
-        this.load.image('asteroid', 'asteroids/asteroid.png'); 
-        this.load.image('small_asteroid', 'asteroids/small_asteroid.png'); 
-        this.load.image('large_asteroid', 'asteroids/large_asteroid.png'); 
-        this.load.spritesheet("dino", 'dino-characters/sheets/dino.png', { frameWidth: 24, frameHeight: 24 });
-    }
-
-    create ()
-    {
-        //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
-        //  For example, you can define global animations here, so we can use them in other scenes.
-
-        //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-        this.scene.start('MainMenu');
-    }
+    //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
+    this.scene.start("MainMenu");
+  }
 }
